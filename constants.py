2beec86f11d5f07224996ae9c0f90f7c3f211f0d
Abhigyani Anand <abhigyani.97@gmail.com>
2021-10-31 03:32:35 +0530

    Location-simulator app

--- constants.py
+++ constants.py
@@ -3,6 +3,5 @@ This file contains all the constants used in this application.
 """
 
 DIRECTION_API = "https://maps.googleapis.com/maps/api/directions/json"
-# Add a valid API_KEY before using the application.
-API_KEY = "" 
+API_KEY = ""
 APP_CONFIG_SECRET_KEY = "0e27ed89b23a2fc7174e6fcfc01ddbdb"
